9e9d124ca1df2fa540462f9f692d1e4370f95bbc
MihSolo[3~ <mihailsologubs@icloud.com>
2024-05-15 00:20:13 +0300

    спринта №9

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,9 +1,22 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import java.time.temporal.TemporalAmount
+import kotlin.concurrent.thread
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +24,115 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
 
         val backToMainSearch = findViewById<Button>(R.id.backToMain)
+        val searchButton = findViewById<ImageView>(R.id.searchButton)
+        val editText = findViewById<EditText>(R.id.inputEditText)
+        val linearLayoutGroupBorder = findViewById<LinearLayout>(R.id.LLGroup)
+        val clearButton = findViewById<ImageView>(R.id.clearButton)
+        var textFromTextField = ""
 
         backToMainSearch.setOnClickListener {
             val backToMainIntent = Intent(this, MainActivity::class.java)
             startActivity(backToMainIntent)
         }
+
+        clearButton.setOnClickListener {
+            editText.setText("")
+        }
+
+        this.addOnContextAvailableListener {
+            editText.setHint("")
+            loadHint {
+                editText.setHint(it)
+            }
+        }
+
+        searchButton.setOnClickListener {
+            val text = "результат поиска по заданному значению: " + textFromTextField
+            Toast.makeText(this@SearchActivity, text, Toast.LENGTH_SHORT).show()
+        }
+
+        val resetTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (clearButton.isVisible.not()) {
+                    val inputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                    inputMethodManager?.hideSoftInputFromWindow(editText.windowToken, 0)
+                }
+            }
+        }
+        editText.addTextChangedListener(resetTextWatcher)
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s.isNullOrEmpty().not()) {
+                    textFromTextField = s.toString()
+                }
+
+            }
+        }
+        editText.addTextChangedListener(searchTextWatcher)
+
+        val textFieldWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                stringWatcherTextEdit = s.toString()
+            }
+        }
+        editText.addTextChangedListener(textFieldWatcher)
+
+
     }
+
+    var stringWatcherTextEdit: String = DEF_VALUE
+
+    companion object {
+        const val AMOUNT_KEY = "AMOUNT_KEY"
+        const val DEF_VALUE = ""
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(AMOUNT_KEY, stringWatcherTextEdit)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        stringWatcherTextEdit = savedInstanceState.getString(AMOUNT_KEY, DEF_VALUE)
+    }
+
+    //------------------
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun loadHint(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            callback.invoke("Поиск")
+        }
+    }
+
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,14 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.content.Intent.createChooser
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.TextView
+import android.widget.Toast
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +16,47 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backToMainSettings = findViewById<Button>(R.id.backToMain)
+        val sharing = findViewById<FrameLayout>(R.id.sharing)
+        val support = findViewById<FrameLayout>(R.id.support)
+        val customerAgreement = findViewById<FrameLayout>(R.id.customerAgreement)
 
         backToMainSettings.setOnClickListener {
             val backToMainIntent = Intent(this, MainActivity::class.java)
             startActivity(backToMainIntent)
         }
+
+        sharing.setOnClickListener {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.androidDeveloper))
+                type = "text/plain"
+            }
+            val chooser: Intent = Intent.createChooser(sendIntent, title)
+            startActivity(chooser)
+        }
+
+        support.setOnClickListener {
+            val shareInt = Intent().apply{
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse("mailto:")
+                type = "ext/plain"
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("mihailsologubs@gmail.com"))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.theme_for_support))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.text_for_support))
+            }
+            startActivity(createChooser(shareInt, title))
+        }
+
+        customerAgreement.setOnClickListener {
+            val agreementIntent = Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = Uri.parse(getString(R.string.customer_agreement))
+            }
+            startActivity(agreementIntent)
+        }
+
+
     }
 }
+
+
